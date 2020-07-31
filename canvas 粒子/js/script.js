c2b652f552ac6987ecf638b6595e2b88d024baba
chenjinhong <1164566601@qq.com>
2020-07-31 12:27:42 +0800

    添加H5 AUDIO

--- canvas 粒子/js/script.js
+++ canvas 粒子/js/script.js
@@ -1,5 +1,29 @@
 // modified version of random-normal
-function normalPool(o){var r=0;do{var a=Math.round(normal({mean:o.mean,dev:o.dev}));if(a<o.pool.length&&a>=0)return o.pool[a];r++}while(r<100)}function randomNormal(o){if(o=Object.assign({mean:0,dev:1,pool:[]},o),Array.isArray(o.pool)&&o.pool.length>0)return normalPool(o);var r,a,n,e,l=o.mean,t=o.dev;do{r=(a=2*Math.random()-1)*a+(n=2*Math.random()-1)*n}while(r>=1);return e=a*Math.sqrt(-2*Math.log(r)/r),t*e+l}
+function normalPool(o) {
+  var r = 0;
+  do {
+      var a = Math.round(normal({
+          mean: o.mean,
+          dev: o.dev
+      }));
+      if (a < o.pool.length && a >= 0) return o.pool[a];
+      r++
+  } while (r < 100)
+}
+
+function randomNormal(o) {
+  if (o = Object.assign({
+          mean: 0,
+          dev: 1,
+          pool: []
+      }, o), Array.isArray(o.pool) && o.pool.length > 0) return normalPool(o);
+  var r, a, n, e, l = o.mean,
+      t = o.dev;
+  do {
+      r = (a = 2 * Math.random() - 1) * a + (n = 2 * Math.random() - 1) * n
+  } while (r >= 1);
+  return e = a * Math.sqrt(-2 * Math.log(r) / r), t * e + l
+}
 
 const NUM_PARTICLES = 600;
 const PARTICLE_SIZE = 0.5; // View heights
@@ -41,16 +65,25 @@ function moveParticle(particle, canvas, time) {
 }
 
 function drawParticle(particle, canvas, ctx) {
+  if(analyser) {
+    analyser.getByteFrequencyData(dataArray);
+    let average = dataArray.reduce((pre,cur) => {
+      return pre + cur
+    }, 0) / bufferLength
+    radious = average /255 * 30 // 区间映射
+  } else {
+    radious = 0
+  }
+
   canvas = document.getElementById('particle-canvas');
   const vh = canvas.height / 100;
-
   ctx.fillStyle = particle.colour;
   ctx.beginPath();
   ctx.ellipse(
     particle.x * canvas.width,
     particle.y * vh + (canvas.height / 2),
-    particle.diameter * vh,
-    particle.diameter * vh,
+    particle.diameter * vh + radious,
+    particle.diameter * vh + radious,
     0,
     0,
     2 * Math.PI
@@ -98,10 +131,51 @@ function startAnimation() {
   for (let i = 0; i < NUM_PARTICLES; i++) {
     particles.push(createParticle(canvas));
   }
+  console.log(particles)
   
   requestAnimationFrame((time) => draw(time, canvas, ctx));
 };
 
+let analyser,dataArray,bufferLength,audioContext
+
+function getWebApi() {
+  const AudioContext = window.AudioContext || window.webkitAudioContext || window.mozAudioContext;
+  audioContext = new AudioContext();//实例化
+  const fileInput = document.getElementById('musicFile')
+  fileInput.addEventListener('change',()=> {
+    const file = document.getElementById('musicFile').files[0]
+    loadMusic(file)
+  })
+}
+
+
+async function loadMusic(file) {
+  const fileBuffer = await loadFile(file)
+  audioContext.decodeAudioData(fileBuffer, function(buffer) {
+    const audioBufferSourceNode = audioContext.createBufferSource();
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 256;
+    audioBufferSourceNode.connect(analyser);
+    analyser.connect(audioContext.destination);
+    audioBufferSourceNode.buffer = buffer;
+    audioBufferSourceNode.start();
+    bufferLength = analyser.frequencyBinCount;
+    dataArray = new Uint8Array(bufferLength)
+    // startAnimation()
+  })
+}
+
+
+function loadFile(file) {
+  return new Promise((resolve) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = function(e) {
+        resolve(e.target.result)
+      }
+    }
+  )
+}
 // Start animation when document is loaded
 (function () {
   if (document.readystate !== 'loading') {
@@ -112,3 +186,7 @@ function startAnimation() {
     })
   }
 }());
+
+window.onload = function() {
+  getWebApi()
+}
