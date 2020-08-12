d39ef48d404bfdb5bcf4f85febb97d92c330b279
cowboy bebop <1164566601@qq.com>
2020-08-12 22:28:10 +0800

    front wall

--- pixie/parallaxScroller/Walls.js
+++ pixie/parallaxScroller/Walls.js
@@ -15,13 +15,15 @@ Walls.VIEWPORT_WIDTH = 512;
 Walls.VIEWPORT_NUM_SLICES = Math.ceil(Walls.VIEWPORT_WIDTH/WallSlice.WIDTH) + 1;
 
 Walls.prototype.setViewportX = function(viewportX) {
-    this.viewportX = this.checkViewportXBounds(viewportX);
+  this.viewportX = this.checkViewportXBounds(viewportX);
 
-    var prevViewportSliceX = this.viewportSliceX;
-    this.viewportSliceX = Math.floor(this.viewportX/WallSlice.WIDTH);
-  };
+  var prevViewportSliceX = this.viewportSliceX;
+  this.viewportSliceX = Math.floor(this.viewportX/WallSlice.WIDTH);
+  this.removeOldSlices(prevViewportSliceX);
+  this.addNewSlices();
+};
 
-  Walls.prototype.checkViewportXBounds = function(viewportX) {
+Walls.prototype.checkViewportXBounds = function(viewportX) {
   var maxViewportX = (this.slices.length - Walls.VIEWPORT_NUM_SLICES) * 
                      WallSlice.WIDTH;
   if (viewportX < 0)
@@ -34,6 +36,48 @@ Walls.prototype.setViewportX = function(viewportX) {
   }
 
   return viewportX;
+};
+
+Walls.prototype.addNewSlices = function() {
+  var firstX = -(this.viewportX % WallSlice.WIDTH);
+  for (var i = this.viewportSliceX, sliceIndex = 0;
+           i < this.viewportSliceX + Walls.VIEWPORT_NUM_SLICES;
+           i++, sliceIndex++)
+  {
+    var slice = this.slices[i];
+    if (slice.sprite == null && slice.type != SliceType.GAP)
+    {
+      slice.sprite = this.borrowWallSprite(slice.type);
+
+      slice.sprite.position.x = firstX + (sliceIndex * WallSlice.WIDTH);
+      slice.sprite.position.y = slice.y;
+
+      this.addChild(slice.sprite);
+    }
+    else if (slice.sprite != null)
+    {
+      slice.sprite.position.x = firstX + (sliceIndex * WallSlice.WIDTH);
+    }
+  }
+};
+
+Walls.prototype.removeOldSlices = function(prevViewportSliceX) {
+  var numOldSlices = this.viewportSliceX - prevViewportSliceX;
+  if (numOldSlices > Walls.VIEWPORT_NUM_SLICES)
+  {
+    numOldSlices = Walls.VIEWPORT_NUM_SLICES;
+  }
+
+  for (var i = prevViewportSliceX; i < prevViewportSliceX + numOldSlices; i++)
+  {
+    var slice = this.slices[i];
+    if (slice.sprite != null)
+    {
+      this.returnWallSprite(slice.type, slice.sprite);
+      this.removeChild(slice.sprite);
+      slice.sprite = null;
+    }
+  }
 };
 
 Walls.prototype.addSlice = function(sliceType, y) {
