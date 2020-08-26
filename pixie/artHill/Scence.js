118d490f4465ea012ed4d05e5430fc3d6b02cf63
cowboy bebop <1164566601@qq.com>
2020-08-26 22:28:32 +0800

    dino jump move

--- pixie/artHill/Scence.js
+++ pixie/artHill/Scence.js
@@ -1,4 +1,8 @@
 function Scence(stage) {
+  this.x = 0;
+  this.y = 0;
+  this.width = 512;
+  this.height = 241;
   this.viewportX = 0;
   this.hills = [];
   this.Dino = null;
@@ -21,12 +25,48 @@ Scence.prototype.setViewportX = function(viewportX) {
   this.viewportX = viewportX;
   let delta = 0.1
   for(let i = 1; i < 6; i ++) {
-    this.hills[i].setViewportX(viewportX, delta += 0.1 )
+    this.hills[i].setViewportX(viewportX, delta += 0.25 )
   }
 };
 
 Scence.prototype.moveViewportXBy = function(units) {
-    this.Dino.loop()
+    this.Dino.move()
+    this.contain()
     let newViewportX = this.viewportX + units;
     this.setViewportX(newViewportX);
 };
+
+Scence.prototype.controlDino = function(vx,direction,flag) {
+  direction = direction || 'right';
+  flag = flag || false;
+  this.Dino.changeDirection(direction)
+  flag&&(this.Dino.changeJump(flag))
+  this.Dino.setVX(vx)
+}
+
+Scence.prototype.controlDinoJump = function(flag) {
+  flag = flag || false;
+  this.Dino.changeJump(flag)
+}
+
+Scence.prototype.contain = function() {
+  if (this.Dino.x < this.x + this.Dino.width/2) {
+    this.Dino.x = this.x + this.Dino.width/2;
+    collision = "left";
+  }
+  //Top
+  if (this.Dino.y < this.y) {
+    this.Dino.y = this.y;
+    collision = "top";
+  }
+  //Right
+  if (this.Dino.x + this.Dino.width/2 > this.width) {
+    this.Dino.x = this.width - this.Dino.width/2;
+    collision = "right";
+  }
+  //Bottom
+  if (this.Dino.y > this.height) {
+    this.Dino.y = this.height;
+    collision = "bottom";
+  }
+}
--- pixie/artHill/dino.js
+++ pixie/artHill/dino.js
@@ -1,20 +1,65 @@
 function dino() {
   PIXI.Container.call(this);
-  this.moveIndex = 0;
-  this.x = 20;
-  this.y = 220;
   this.dinoId = resources["./DinoSpriteSheet.json"].textures;
   this.dinoSprite = null;
+  this.moveIndex = 0;
+  this.x = 20;
+  this.y = 241;
+  this.vx = 0;
+  this.vy = 0;
+  this.direction = 'right'
+  this.isJump = false
 }
 
 dino.prototype = Object.create(PIXI.Container.prototype);
 
 dino.MOVEDELTA = 0.2
+dino.JUMPSPEED = 6
+dino.GRAVITY = 0.25
 
-dino.prototype.loop = function() {
+dino.prototype.setX = function(x) {
+  this.x = x;
+}
+
+dino.prototype.setY = function(y) {
+  this.y = y;
+}
+
+dino.prototype.Moveloop = function() {
   if(this.moveIndex > 10) this.moveIndex = 4;
   this.moveIndex += dino.MOVEDELTA;
   this.removeChild(this.dinoSprite);
   this.dinoSprite = new Sprite(this.dinoId[`DinoSpriteSheet ${parseInt(this.moveIndex)}.png`]);
+  this.dinoSprite.width = 24
+  this.dinoSprite.height = 24
+  this.dinoSprite.pivot.x = this.dinoSprite.width / 2;
+  this.dinoSprite.pivot.y = this.dinoSprite.height;
+  this.direction == 'left' ? this.dinoSprite.scale.x = -1 : '';
   this.addChild(this.dinoSprite);
+}
+
+dino.prototype.move = function() {
+  this.Moveloop()
+  this.x += this.vx;
+  if(this.isJump) this.y -= (dino.JUMPSPEED -= dino.GRAVITY);
+  if (this.y >= 241) {
+      this.y = 241
+      dino.JUMPSPEED = 6
+      this.isJump = false
+  }
+}
+
+dino.prototype.autoMove = function() {
+  this.Moveloop()
+}
+
+dino.prototype.setVX = function(vx) {
+  this.vx = vx;
+}
+
+dino.prototype.changeDirection = function(direction) {
+  this.direction = direction
+}
+dino.prototype.changeJump = function(flag) {
+  this.isJump = flag
 }
