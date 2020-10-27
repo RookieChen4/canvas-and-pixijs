6cda1f32069b4e7b44dbb649a500ead048e8c6c6
chenjinhong <1164566601@qq.com>
2020-10-27 19:41:13 +0800

    music 7

--- music/index.js
+++ music/index.js
@@ -107,10 +107,13 @@ function renderTag() {
 
 let lineWidth = 15
 let radius = canvas.width * 0.2
+let pointRadius = 20
 const PI2 = Math.PI * 2
 let progressPercent = 0
 let progressPointX = 0;
 let progressPointY = 0;
+let isOnProgressPoint = false
+let moveProgressPoint = false
 function renderProgress() {
     ctx.beginPath();
     ctx.strokeStyle = '#e9e9e9';
@@ -119,14 +122,21 @@ function renderProgress() {
     ctx.stroke();
 
 
-    const { duration, currentTime } = audio;
-    progressPercent = (currentTime / duration) * PI2
+    if(!moveProgressPoint) {
+        const { duration, currentTime } = audio;
+        progressPercent = (currentTime / duration) * PI2
+    }
     ctx.beginPath();
     ctx.strokeStyle = 'red';
     ctx.lineWidth = lineWidth; 
     ctx.arc(canvas.width - radius - padding * 10, canvas.height/2, radius,0,progressPercent); 
     ctx.stroke();
     
+
+    progressPointX = canvas.width - radius - padding * 10 + radius*Math.cos(progressPercent)
+    progressPointY = canvas.height/2 + radius*Math.sin(progressPercent)
+    isOnProgressPoint = isInCircle(progressPointX,progressPointY,pointRadius,...translateMouse(x,y))
+    pointRadius = isOnProgressPoint ? 30 : 20 
     ctx.translate(0,0)
     ctx.rotate(0)
     ctx.save()
@@ -134,15 +144,13 @@ function renderProgress() {
     ctx.rotate(progressPercent);
     ctx.beginPath();
     ctx.fillStyle = 'red';
-    ctx.arc(radius, 0, 20,0,PI2); 
+    ctx.arc(radius, 0, pointRadius,0,PI2); 
     ctx.fill();
     ctx.beginPath();
     ctx.fillStyle = 'white';
-    ctx.arc(radius, 0, 10,0,PI2); 
+    ctx.arc(radius, 0, pointRadius/2,0,PI2); 
     ctx.fill();
     ctx.restore()
-    progressPointX = canvas.width - radius - padding * 10 + radius*Math.cos(progressPercent)
-    progressPointY = canvas.height/2 + radius*Math.sin(progressPercent)
 }
 
 function renderVisualize() {
@@ -183,14 +191,14 @@ async function draw() {
 getImg()
 
 
-canvas.addEventListener('click',() => {
-    // let index = Math.floor(Math.random()*10)
-    PlayIndex = (PlayIndex + 1) % playList.length
-    findSongUrl(playList[PlayIndex].id)
-    audio.play();
-    // let imgurl = imgList[index].urls.full
-    // container.style.backgroundImage = `url(${imgurl})`;
-})
+// canvas.addEventListener('click',() => {
+//     // let index = Math.floor(Math.random()*10)
+//     PlayIndex = (PlayIndex + 1) % playList.length
+//     findSongUrl(playList[PlayIndex].id)
+//     audio.play();
+//     // let imgurl = imgList[index].urls.full
+//     // container.style.backgroundImage = `url(${imgurl})`;
+// })
 
 // audio.addEventListener('timeupdate', updateProgress);
 
@@ -211,12 +219,36 @@ window.addEventListener('resize', () => {
     // ctx = canvas.getContext("2d");
 })
 
-
+let centerX = canvas.width - radius - padding * 10
+let centerY = canvas.height/2
 canvas.addEventListener('mousemove', e => {
     x = e.offsetX;
     y = e.offsetY;
+    if(moveProgressPoint) {
+        const [x1,y1] = translateMouse(x,y)
+        let angle = calcAngleDegrees(x1 - centerX, y1 - centerY)
+        if(angle < 0) {
+            audio.currentTime = audio.duration * (360 + angle)/360
+            progressPercent = (360 + angle)/360 * PI2
+        } else {
+            audio.currentTime = audio.duration * angle/360
+            progressPercent = angle/360 * PI2
+        }
+    }
 });
 
+canvas.addEventListener('mousedown', e => {
+    if(isOnProgressPoint) {
+        moveProgressPoint = true
+    }
+});
+
+canvas.addEventListener('mouseup', e => {
+    // if(moveProgressPoint) {
+    //     // audio.duration = 
+    // }
+    moveProgressPoint = false
+});
 
 let source,gainNode;
 source = audioContext.createMediaElementSource(audio);
@@ -234,14 +266,14 @@ audio.addEventListener('play', () => {
     // Get new mouse pointer coordinates when mouse is moved
     // then set new gain value
   
-    document.onmousemove = updatePage;
+    // document.onmousemove = updatePage;
   
-    function updatePage(e) {
-        CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
+    // function updatePage(e) {
+    //     CurY = (window.Event) ? e.pageY : event.clientY + (document.documentElement.scrollTop ? document.documentElement.scrollTop : document.body.scrollTop);
   
-        gainNode.gain.value = CurY/HEIGHT;
-        audio.volume = CurY/HEIGHT;
-    }
+    //     gainNode.gain.value = CurY/HEIGHT;
+    //     audio.volume = CurY/HEIGHT;
+    // }
   
   
     // connect the AudioBufferSourceNode to the gainNode
@@ -263,5 +295,86 @@ function isInCircle(x1,y1,r,x2,y2) {
         return true
     }else {
         return false
+    }
+}
+
+function translateMouse(x,y) {
+    let q = canvas.width/window.innerWidth*x
+    let b = canvas.height/window.innerHeight*y
+    return [q,b]
+}
+
+
+function calculateAngle(x1,y1,x2,y2) {
+    // 第一象限
+    if(x1>x2&&y1>y2) {
+        console.log(1)
+    }
+    // 第二象限
+    if(x1<x2&&y1>y2) {
+        console.log(2)
+    }
+    // 第三象限
+    if(x1<x2&&y1<y2) {
+        console.log(3)
+    }
+    // 第四象限
+    if(x1>x2&&y1<y2) {
+        console.log(4)
+    }
+}
+
+function calcAngleDegrees(x, y) {
+    return Math.atan2(y, x) * 180 / Math.PI;
+}
+
+window.onload = function() {
+    let control = document.getElementsByClassName('control')[0]
+    control.style.top = centerY * window.innerHeight /canvas.height + 'px'
+    control.style.left = 100 * window.innerWidth /canvas.width + 'px'
+    let play = document.getElementById('play')
+    let pause = document.getElementById('pause')
+    let next = document.getElementById('next')
+    let pre = document.getElementById('pre')
+    play.addEventListener('click',() => {
+        audio.play();
+        play.style.display = 'none'
+        pause.style.display = 'block'
+    },false)
+
+    pause.addEventListener('click',() => {
+        audio.pause();
+        pause.style.display = 'none'
+        play.style.display = 'block'
+    },false)
+
+    next.addEventListener('click',() => {
+        PlayIndex = (PlayIndex + 1) % playList.length
+        findSongUrl(playList[PlayIndex].id)
+        audio.play();
+        play.style.display = 'none'
+        pause.style.display = 'block'
+    },false)
+
+    pre.addEventListener('click',() => {
+        PlayIndex = (PlayIndex - 1) % playList.length
+        if(PlayIndex < 0) {
+            PlayIndex = 0
+        }
+        findSongUrl(playList[PlayIndex].id)
+        audio.play();
+        play.style.display = 'none'
+        pause.style.display = 'block'
+    },false)
+
+    document.addEventListener('wheel',scrollVolume, false)
+    function scrollVolume(e) {
+        if(e.deltaY > 0) {
+            // gainNode.gain.value -= 0.05;
+            audio.volume -= 0.05;
+        }else {
+            // gainNode.gain.value += 0.05;
+            audio.volume += 0.05;
+        }
     }
 }
