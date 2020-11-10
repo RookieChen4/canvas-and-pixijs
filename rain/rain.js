4518a31127dcce65b26e601921418926cf402454
cowboy bebop <1164566601@qq.com>
2020-11-10 22:46:05 +0800

    rain

--- rain/rain.js
+++ rain/rain.js
@@ -2,4 +2,17 @@ class Rain {
   constructor() {
     
   }
+  splash() {
+    if (!this.splashed) {
+      this.splashed = true;
+      var drops = demo.drops;
+      var drop_pool = demo.drop_pool;
+  
+      for (var i=0; i<16; i++) {
+        var drop = drop_pool.pop() || new Drop();
+        drops.push(drop);
+        drop.init(this.x);
+      }
+    }
+  }
 }
